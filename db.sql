fc1304043cc63f0807785fb3107a3929178a9c73
pabloOcanto <pomalianni@gmail.com>
2021-05-12 23:06:04 -0300

    agregado de usuario tipos choferes y agregado header allo-origin

--- db.sql
+++ db.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS `transtour.roles`(
+CREATE TABLE IF NOT EXISTS roles(
     id int NOT NULL AUTO_INCREMENT,
     name varchar(50) NOT NULL,
     CONSTRAINT role_name UNIQUE (name),
@@ -6,10 +6,10 @@ CREATE TABLE IF NOT EXISTS `transtour.roles`(
 )  ENGINE=InnoDB;
 
 
-CREATE TABLE IF NOT EXISTS `transtour.users`(
+CREATE TABLE IF NOT EXISTS users(
     id int NOT NULL AUTO_INCREMENT,
     user_name varchar(50) NOT NULL ,
-    password varchar(10) NOT NULL ,
+    password varchar(100) NOT NULL ,
     first_name varchar(50),
     last_name varchar(50),
     enabled  bit(1) NOT NULL DEFAULT 0,
@@ -17,7 +17,7 @@ CREATE TABLE IF NOT EXISTS `transtour.users`(
     CONSTRAINT pk_user_id PRIMARY KEY (id)
 )  ENGINE=InnoDB;
 
-CREATE TABLE IF NOT EXISTS `transtour.user_role`(
+CREATE TABLE IF NOT EXISTS user_role(
     id int NOT NULL AUTO_INCREMENT,
     user_id int NOT NULL,
     role_id int NOT NULL,
@@ -26,20 +26,44 @@ CREATE TABLE IF NOT EXISTS `transtour.user_role`(
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (role_id) REFERENCES roles(id)
 
-)  ENGINE=InnoDB;
+)ENGINE=InnoDB;
 
 
-ALTER TABLE `transtour.users`
-ADD UNIQUE INDEX username_index (username ASC) ;
+ALTER TABLE users
+ADD UNIQUE INDEX username_index (user_name ASC) ;
 
-INSERT INTO `users` (`user_name`, `password`, `first_name`, `last_name`, `enabled`)
-VALUES ('kike.transtour@gmail.com', '1A_23#', 'kike','apellido',1)
-
-INSERT INTO `roles` (`name`)
+INSERT INTO roles (`name`)
 VALUES ('ROLE_ADMIN')
 
-INSERT INTO `user_role` (`user_id`,`role_id`)
+INSERT INTO roles (`name`)
+VALUES ('ROLE_USER')
+
+INSERT INTO roles (`name`)
+VALUES ('ROLE_DRIVER')
+
+
+
+INSERT INTO users (`user_name`, `password`, `first_name`, `last_name`, `enabled`)
+VALUES ('kike.transtour@gmail.com', 'gpw5LY9MmjOEmQCOu25TzONp1mm4eSjsEfEmCxsFKnkldLE4qJbeO', 'kike','apellido',1)
+
+INSERT INTO users (`user_name`, `password`, `first_name`, `last_name`, `enabled`)
+VALUES ('carlos.transtour@gmail.com', 'gpw5LY9MmjOEmQCOu25TzONp1mm4eSjsEfEmCxsFKnkldLE4qJbeO', 'carlos','lfws',3)
+
+
+
+INSERT INTO users (`user_name`, `password`, `first_name`, `last_name`, `enabled`)
+VALUES ('pablo.transtour@gmail.com', 'gpw5LY9MmjOEmQCOu25TzONp1mm4eSjsEfEmCxsFKnkldLE4qJbeO', 'pablo','lfws',3)
+
+
+
+INSERT INTO user_role (`user_id`,`role_id`)
 VALUES (1,1)
+
+INSERT INTO user_role (`user_id`,`role_id`)
+VALUES (2,3)
+
+INSERT INTO user_role (`user_id`,`role_id`)
+VALUES (3,3)
 
 commit;
 
